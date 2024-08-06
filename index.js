778683110bed6ff7474728dcf09a64815c2d8bd8
MimFSD <mimislam878@gmail.com>
2024-08-06 12:00:19 +0600

    delete functionality starded

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kj2w8eq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions 
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -72,7 +72,7 @@ async function run() {
       const result = await UserCraftCollection.findOne(query);
       res.send(result);
     });
-    
+
     app.get('/update/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
