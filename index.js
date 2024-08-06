6f8882f7d5c8a28bac005b3e71dce348dd5e792c
MimFSD <mimislam878@gmail.com>
2024-08-06 12:01:59 +0600

    update for data get starded

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	
+    // Connect the client to the server	(optional starting in v4.7)
 
     const UserCraftCollection = client
       .db('ArtisticAlchemy')
